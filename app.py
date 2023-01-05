8287091221e45e88408bf7a76f063b292d37e989
Luqmaan Hakeem <rockgameplayhakeem@gmail.com>
2023-01-05 11:26:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import render_template , Flask , jsonify ,request
+import sqlite3
+DB_PATH='teams.db'
+app = Flask(__name__, static_folder='static')
+app.template_folder = 'templates'
+
+@app.route('/')
+def hello():
+    return render_template("index.html")
+
+@app.route('/api/teams')
+def getteams():
+    team=[{'id':1,'name':"PSG"},{'id':2,'name':"liverpool"},{'id':3,'name':"Man City"}]
+    return jsonify(team)
+#@app.route('/search', methods=['POST'])
+#def getteam():
+    team=[{'id':1,'name':"PSG","Location":"France"},{'id':2,'name':"liverpool","Location":"England"},{'id':3,'name':"Man City","Location":"England"}]
+    result =[]
+    input_req=request.json['searchTerm']
+    for teams in team:
+        if input_req.lower() in teams['name'].lower():
+            result.append(teams)
+            
+    return result  
+
+@app.route('/search', methods=['POST'])
+def search():
+  search_term = request.json['searchTerm']
+  results = search_teams(search_term)
+  return jsonify(results)
+
+def search_teams(search_term):
+    conn = sqlite3.connect(DB_PATH)
+    c= conn.cursor()
+    c.execute("SELECT * FROM teams WHERE name LIKE ?", (f'%{search_term}%',))
+    results = c.fetchall()
+    conn.close()
+    return results
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
---
+++ static/main.js
@@ -0,0 +1,29 @@
+ // Get a reference to the search form
+ const form = document.getElementById('search-form');
+
+      
+ 
+ form.addEventListener('submit', event => {
+   event.preventDefault();
+
+  
+   const searchTerm = document.getElementById('team-name').value;
+
+
+   fetch('/search', {
+     method: 'POST',
+     body: JSON.stringify({ searchTerm }),
+     headers: { 'Content-Type': 'application/json' },
+   })
+     .then(response => response.json()) 
+     .then(data => {
+      const pre = document.createElement('result');
+      pre.textContent = JSON.stringify(data, null, 2);
+    
+      document.body.appendChild(pre);
+    
+
+      
+     });
+     
+ });
